6455ce2d6766e2b1b727974f08f0b93dd9d9236a
yx420 <122374639+yx420@users.noreply.github.com>
2023-11-01 23:55:39 +0800

    Home and navbar

--- vue-project/src/components/NavBar.vue
+++ vue-project/src/components/NavBar.vue
@@ -1,88 +1,81 @@
 <template>
-    <nav class="nav-bar">
-      <div class="nav-logo">
-        <img src="@/assets/Logo.png" alt="PillPal Logo" />
-      </div>
-      <div class="nav-menu">
-        <button class="nav-button">
-          Home
-        </button>
-        <button class="nav-button">
-          Reminder
-        </button>
-        <button class="nav-button">
-          Locator
-        </button>
-        <button class="nav-button">
-          Profile
-        </button>
-      </div>
-      <div class="nav-about">
-        <button class="nav-button" @click="goToAbout">
-          About Us
-        </button>
-      </div>
-    </nav>
-  </template>
-  
-  <script>
-  export default {
-    methods: {
-      goToAbout() {
-        // Add your route navigation logic to About page here
-      },
+  <nav class="nav-bar">
+    <div class="nav-logo">
+      <img src="@/assets/Logo.png" alt="PillPal Logo" />
+    </div>
+    <div class="nav-menu">
+      <button class="nav-button">Home</button>
+      <button class="nav-button">Reminder</button>
+      <button class="nav-button">Locator</button>
+      <button class="nav-button">Profile</button>
+      <button class="nav-button">LogOut</button>
+    </div>
+    <div class="nav-about">
+      <button class="nav-button" @click="goToAbout">About Us</button>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  methods: {
+    goToAbout() {
+      // Add your route navigation logic to About page here
     },
-  };
-  </script>
-  
-  <style scoped>
-  .nav-bar {
-    width: 35px;
-    height: 324px;
-    flex-shrink: 0;
-    background: #3CB26D;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-    padding: 20px 0;
-  }
-  
-  .nav-logo img {
-    width: 27.327px;
-    height: 28.086px;
-  }
-  
-  .nav-button {
-    width: 30.364px;
-    height: 29.09px;
-    flex-shrink: 0;
-    border-radius: 0px 4px 4px 0px;
-    background: #FFF;
-    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-    text-align: center;
-    color: #3CB26D;
-    font-family: Kanit;
-    font-size: 6px;
-    font-weight: 500;
-    text-decoration: none;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-  }
-  
-  .nav-about .nav-button {
-    width: 26.315px;
-    height: 4.012px;
-    border-radius: 0px;
-    text-decoration: underline;
-    font-size: 6px;
-  }
-  
-  .nav-button:hover {
-    background: #3CB26D;
-    color: #FFF;
-  }
-  </style>
-  
+  },
+};
+</script>
+
+<style scoped>
+.nav-bar {
+  background: #3cb26d;
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+}
+
+.nav-logo img {
+  width: 70px;
+  height: 70px;
+  background-color: #fff;
+}
+
+.nav-menu {
+  display: flex;
+  flex-direction: column; /* Arrange the buttons vertically */
+  align-items: left;
+}
+
+.nav-button {
+  width: 70px;
+  height: 70px;
+  flex-shrink: 0;
+  border-radius: 0px 4px 4px 0px;
+  background: #fff;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  text-align: center;
+  color: #3cb26d;
+  font-family: Kanit;
+  font-size: 6px;
+  font-weight: 500;
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  margin: 4px 0; /* Add vertical margin between buttons */
+}
+
+.nav-about .nav-button {
+  width: 60px;
+  height: 10px;
+  border-radius: 0px;
+  text-decoration: underline;
+  font-size: 6px;
+}
+
+.nav-button:hover {
+  background: #3cb26d;
+  color: #fff;
+}
+</style>
--- vue-project/src/views/Home.vue
+++ vue-project/src/views/Home.vue
@@ -1,21 +1,22 @@
 <template>
   <div class="home-page">
-    <img src="@/assets/HOME.png" alt="Home Image" />
-    <div class="quote">
-      <p class="quote-text">A SINGLE SOURCE SOLUTION</p>
-      <p class="quote-text">TO YOUR HEALTH NEEDS</p>
+    <NavBar />
+    <div class="content">
+      <div class="quote">
+        <p class="quote-text">A SINGLE SOURCE SOLUTION</p>
+        <p class="quote-text">TO YOUR HEALTH NEEDS</p>
+      </div>
     </div>
-    <nav-bar></nav-bar>
   </div>
 </template>
 
 <script>
-import NavBar from "@/components/NavBar.vue";
+import NavBar from "@/components/NavBar.vue"; // Import the NavBar component
 
 export default {
   name: "Home",
   components: {
-    NavBar,
+    NavBar, // Register the NavBar component
   },
 };
 </script>
@@ -23,30 +24,39 @@ export default {
 <style scoped>
 .home-page {
   display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  background-size: cover;
+  flex-direction: row;
+  align-items: stretch;
   min-height: 100vh;
   color: #fff;
+  overflow: hidden;
+}
+
+.content {
+  flex: 1;
+  background: url(@/assets/HOME.png) no-repeat center center;
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  padding: 20px;
 }
 
 .quote {
-  text-align: left;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  text-align: right;
   font-family: Kanit;
-  font-size: 20px;
+  font-size: 60px;
   font-weight: 600;
   line-height: normal;
-  position: absolute;
-  bottom: 10px;
-  left: 10px;
+  color: #fff;
+  padding: 20px;
 }
 
 .quote-text {
   margin: 0;
-}
-
-.quote-text:first-child {
-  color: #3cb26d;
 }
 </style>
