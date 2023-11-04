8fcac8198ab66452d7e82cff6b5e29da3e6a3507
Low Mei Lin <lmlmeilin@gmail.com>
2023-11-04 18:55:47 +0800

    Edit Add Reminder component

--- vue-project/src/components/AddReminder.vue
+++ vue-project/src/components/AddReminder.vue
@@ -1,36 +1,37 @@
 <template>
-  <div class ="container">
-      <form id="myform">
-          <h2>Add New Reminder, #Reminder X</h2>
-
-          <div class = "formli" >            
-              <label for="med">Medicine Name:</label>
-              <input type="text" id = "med" required ="" placeholder="Enter your med"> <br><br>              
-              <label for="dosage">Dosage: </label>
-              <input type="number" id = "dosage" required =""  placeholder="Enter dosage"><br><br>               
-              <label for="freq">How many times do you take this medicine per day? </label>
-              <input type="number" id = "freq" required =""  placeholder="Enter number of times" ><br><br>
-              <label for="baFood">Before/After Food </label>
-              <input type="text" id = "baFood" required =""  placeholder="Select before/after food" ><br><br>
-              <label for="setRem">Set reminder notification to ring/silent</label>
-              <input type="text" id = "setRem" required =""  placeholder="Select ring/silent" ><br><br>
-              <label for="chooseFreq">Choose frequency of intake</label>
-              <input type="text" id = "chooseFreq" required =""  placeholder="Daily/WeekLy/Monthly" ><br><br>
-              <label for="first">1st Intake</label>
-              <input type="number" id = "first" required =""  placeholder="Enter time" ><br><br>
-              <label for="second">2nd Intake</label>
-              <input type="number" id = "second" required =""  placeholder="Enter time" ><br><br>
-              <label for="third">3rd Intake</label>
-              <input type="number" id = "third" required =""  placeholder="Enter time" ><br><br>
-              <label for="totalDuration">Total duration required to complete the medication</label>
-              <input type="number" id = "totalDuration" required =""  placeholder="Enter duration" ><br><br>
-              
-              <div class ="createReminder" >
-                  <button  id = "createReminderButton"  type="button"  v-on:click="savetofs"> Create Reminder </button>
-             </div>
-          </div>
-      </form> 
-  </div>
+ <div class ="container">
+        <form id="myform">
+            <h1>Create New Reminder</h1>
+            <h2>Add New Reminder, Reminder #X</h2>
+  
+            <div class = "formli" >            
+                <label for="med">Medicine Name</label>
+                <input type="text" id = "med" required ="" placeholder="Enter medicine name"> <br><br>              
+                <label for="dosage">Dosage</label>
+                <input type="number" id = "dosage" required =""  placeholder="Enter dosage"><br><br>               
+                <label for="freq">How many times do you take this medicine per day? </label>
+                <input type="number" id = "freq" required =""  placeholder="Enter number of times" ><br><br>
+                <label for="baFood">Before/After Food </label>
+                <input type="text" id = "baFood" required =""  placeholder="Select before/after food" ><br><br>
+                <label for="setRem">Set reminder notification to ring/silent</label>
+                <input type="text" id = "setRem" required =""  placeholder="Select ring/silent" ><br><br>
+                <label for="chooseFreq">Choose frequency of intake</label>
+                <input type="text" id = "chooseFreq" required =""  placeholder="Daily/Weekly/Monthly" ><br><br>
+                <label for="first">1st Intake</label>
+                <input type="number" id = "first" required =""  placeholder="Enter time" ><br><br>
+                <label for="second">2nd Intake</label>
+                <input type="number" id = "second" required =""  placeholder="Enter time" ><br><br>
+                <label for="third">3rd Intake</label>
+                <input type="number" id = "third" required =""  placeholder="Enter time" ><br><br>
+                <label for="totalDuration">Total duration required to complete the medication</label>
+                <input type="number" id = "totalDuration" required =""  placeholder="Enter duration" ><br><br>
+                
+                <div class ="createReminder" >
+                    <button  id = "createReminderButton"  type="button"  v-on:click="savetofs"> Create Reminder </button>
+               </div>
+            </div>
+        </form> 
+    </div>
 </template>
 
 
