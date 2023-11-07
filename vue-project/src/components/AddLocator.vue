59f156c6c5b8bcc51b2dd170038e4336d109a5aa
Xuehann <e0969691@u.nus.edu>
2023-11-07 22:42:47 +0800

    Update Clinic Locator
    Added in router and converted to Vue component

---
+++ vue-project/src/components/AddLocator.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <h3>Clinic Locator</h3>
+    <div>
+      <input id="searchInput" v-model="searchQuery" placeholder="Search clinics" @input="updateClinicList" />
+      <select id="medicineFilter" v-model="selectedMedicine" @change="updateClinicList">
+        <option value="">All Medicines</option>
+        <option value="Medicine A">Medicine A</option>
+        <option value="Medicine B">Medicine B</option>
+        <option value="Medicine C">Medicine C</option>
+        <!-- Add more filter options as needed -->
+      </select>
+      <select id="holidayFilter" v-model="selectedHolidayFilter" @change="updateClinicList">
+        <option value="">All Clinics</option>
+        <option value="Open on Holidays">Open on Holidays</option>
+        <option value="Open on Sunday">Open on Sunday</option>
+        <option value="Open 24 Hours">Open 24 Hours</option>
+        <!-- Add more filter options as needed -->
+      </select>
+    </div>
+    <div id="clinic-list">
+      <div v-for="clinic in filteredClinics" :key="clinic.name">
+        <h2>{{ clinic.name }}</h2>
+        <p>Location: {{ clinic.location }}</p>
+        <p>Medicine: {{ Array.isArray(clinic.medicine) ? clinic.medicine.join(', ') : clinic.medicine }}</p>
+        <p>Open on Holidays: {{ clinic.openOnHolidays ? 'Yes' : 'No' }}</p>
+        <p>Open on Sunday: {{ clinic.openOnSunday ? 'Yes' : 'No' }}</p>
+        <p>Open 24 Hours: {{ clinic.openAllday ? 'Yes' : 'No' }}</p>
+      </div>
+    </div>
+    <div id="map" style="height: 400px; width: 100%;"></div>
+  </div>
+</template>
+
+<script>
+// You need to convert your external JS to a module or inline script here.
+export default {
+  name: 'AddLocator',
+  data() {
+    return {
+      searchQuery: '',
+      selectedMedicine: '',
+      selectedHolidayFilter: '',
+      clinics: [
+        // ... your clinics data ...
+      ],
+      filteredClinics: [],
+    };
+  },
+  methods: {
+    updateClinicList() {
+      this.filteredClinics = this.clinics.filter((clinic) => {
+        const medicineCondition = this.selectedMedicine === "" || clinic.medicine.includes(this.selectedMedicine);
+        const holidayCondition = this.selectedHolidayFilter === "" || 
+          (this.selectedHolidayFilter === "Open on Holidays" && clinic.openOnHolidays) ||
+          (this.selectedHolidayFilter === "Open on Sunday" && clinic.openOnSunday) ||
+          (this.selectedHolidayFilter === "Open 24 Hours" && clinic.openAllday);
+
+        return (
+          clinic.name.toLowerCase().includes(this.searchQuery.toLowerCase()) &&
+          medicineCondition &&
+          holidayCondition
+        );
+      });
+    },
+  },
+  mounted() {
+    this.updateClinicList();
+    // Initialization code for the map should go here.
+    // You may need to convert your vanilla JS map initialization code
+    // to Vue's approach, possibly using Vue's lifecycle hooks.
+  },
+}
+</script>
+
+<style scoped>
+#map {
+  height: 300px;
+  width: 100%;
+  margin: auto;
+}
+
+h3 {
+  background-color: #4CAF50; 
+  color: white; 
+  padding: 10px; 
+}
+</style>
---
+++ vue-project/src/views/Locator.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class = "container">
+        <NavBar />
+        <AddLocator />
+    </div>
+
+</template>
+
+<script>
+    import NavBar from "@/components/NavBar.vue";
+    import AddLocator from "@/components/AddLocator.vue";
+    import { initMap } from "/Users/xuehan/Desktop/final-project-l1-final-project-group-5/vue-project/src/clinicLocate.js";
+
+    export default {
+        name: "Locator",
+        components: {
+            NavBar, 
+            AddLocator,
+        },
+
+        mounted() {
+        initMap().catch(error => {
+        console.error("Map initialization failed:", error);
+    });
+  },
+
+
+    }
+</script>
