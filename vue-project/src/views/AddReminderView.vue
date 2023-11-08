f448ca159f8a24deb7218c6c9ac5b31365e0bd44
yx420 <122374639+yx420@users.noreply.github.com>
2023-11-08 17:06:45 +0800

    add reminder title css

--- vue-project/src/views/AddReminderView.vue
+++ vue-project/src/views/AddReminderView.vue
@@ -1,45 +1,78 @@
 <template>
-    <div class = "container">
-        <NavBar class="nav-bar" />
-        <div class="content">
-        <AddReminder @added="change"/>
-        <TableFormat :key="refreshComp"/>  
-        </div>
+  <div class="container">
+    <NavBar class="nav-bar" />
+    <div class="content">
+      <div class="title">
+        <h1 class="header">Create New Reminder</h1>
+      </div>
+      <div class="add-reminder-box">
+        <AddReminder @added="change" />
+      </div>
+      <TableFormat :key="refreshComp" />
     </div>
-  </template>
-  
-  <script>
-  import NavBar from '@/components/NavBar.vue'
-  import AddReminder from '@/components/AddReminder.vue'
-  import  TableFormat from '@/components/TableFormat.vue'
-  
-  export default {
-    name: 'addReminderView',
-    components:{
-        NavBar, 
-        AddReminder,
-        TableFormat,
+  </div>
+</template>
+
+<script>
+import NavBar from "@/components/NavBar.vue";
+import AddReminder from "@/components/AddReminder.vue";
+import TableFormat from "@/components/TableFormat.vue";
+
+export default {
+  name: "addReminderView",
+  components: {
+    NavBar,
+    AddReminder,
+    TableFormat,
+  },
+  data() {
+    return {
+      refreshComp: 0,
+    };
+  },
+  methods: {
+    change() {
+      this.refreshComp += 1;
     },
-    data(){
-      return{
-        refreshComp: 0
-      }
-    },
-    methods:{
-      change(){  
-        this.refreshComp += 1
-      }
-    },  
-  }
-  </script>
-  
-  <style>
-.nav-bar {
-  width: 100%;
-  height: 100%;
-  /* Add other styling for the NavBar as needed */
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: row;
+  align-items: stretch;
+  min-height: 100vh;
+  background: white;
 }
-
-
-  </style>
-  
+.content {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: stretch;
+  text-align: center;
+  padding: 20px;
+  overflow-y: auto;
+  height: max-content;
+}
+.header {
+  font-size: 30px;
+  font-weight: bold;
+  background: #3cb26d;
+  color: white;
+  padding: 20px;
+  margin: 10px;
+  border-radius: 10px;
+  margin-left: 0;
+}
+.add-reminder-box {
+  background: white;
+  padding: 20px;
+  border-radius: 10px;
+  border: 2px solid #3cb26d;
+  flex-grow: 1;
+  height: max-content;
+}
+</style>
